176c3e7539c0ac8dcf0a75edfdbe9afe4b1280a3
Martin Lönnqvist <martin.lonnqvist@live.se>
2026-05-15 16:14:18 +0200

    sdfsafd

--- SynthesisRPGLoot/Analyzers/GearAnalyzer.cs
+++ SynthesisRPGLoot/Analyzers/GearAnalyzer.cs
@@ -399,7 +399,8 @@ namespace SynthesisRPGLoot.Analyzers
             {
                 
                 var ench = pool[i];
-                Console.WriteLine(ench.Enchantment.Name + " " + ench.Level + " " + ench.Enchantment.EnchantmentAmount);
+                if (ench.Enchantment.Name.ToString() == "Fortify Destruction") 
+                    Console.WriteLine(ench.Enchantment.Name + " " + ench.Level + " " + ench.Enchantment.EnchantmentAmount);
                 
                 double weight = 1.0;
 
